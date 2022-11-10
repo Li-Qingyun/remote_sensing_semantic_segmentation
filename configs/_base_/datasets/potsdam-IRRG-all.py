194bb1c263830c929246a3dbc702dc3acc1640c1
QingyunLi <962537281@qq.com>
2022-11-10 09:03:20 +0800

    fix problem of ignore_index

--- configs/_base_/datasets/potsdam-IRRG-all.py
+++ configs/_base_/datasets/potsdam-IRRG-all.py
@@ -1,10 +1,14 @@
 _base_ = './potsdam.py'
 # dataset settings
+ignore_index = 5
 train_dataloader = dict(
     dataset=dict(
         data_prefix=dict(
-            img_path='img_IRRG/train', seg_map_path='ann_all/train')))
+            img_path='img_IRRG/train', seg_map_path='ann_all/train'),
+        ignore_index=ignore_index))
 val_dataloader = dict(
     dataset=dict(
         data_prefix=dict(
-            img_path='img_IRRG/val', seg_map_path='ann_all/val')))
+            img_path='img_IRRG/val', seg_map_path='ann_all/val')),
+    ignore_index=ignore_index)
+test_dataloader = val_dataloader
