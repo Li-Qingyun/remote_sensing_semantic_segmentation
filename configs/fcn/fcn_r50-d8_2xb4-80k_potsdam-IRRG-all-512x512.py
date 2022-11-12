54f6db63f356b1fb413a9450b49fc75cd42f81fc
QingyunLi <962537281@qq.com>
2022-11-12 15:38:09 +0800

    fix: add ignore_index in heads

--- configs/fcn/fcn_r50-d8_2xb4-80k_potsdam-IRRG-all-512x512.py
+++ configs/fcn/fcn_r50-d8_2xb4-80k_potsdam-IRRG-all-512x512.py
@@ -2,11 +2,12 @@ _base_ = [
     '../_base_/models/fcn_r50-d8.py', '../_base_/datasets/potsdam-IRRG-all.py',
     '../_base_/default_runtime.py', '../_base_/schedules/schedule_80k.py'
 ]
+ignore_index = 5
 crop_size = (512, 512)
 data_preprocessor = dict(size=crop_size)
 model = dict(
     data_preprocessor=data_preprocessor,
-    decode_head=dict(num_classes=5),
-    auxiliary_head=dict(num_classes=5))
+    decode_head=dict(num_classes=5, ignore_index=ignore_index),
+    auxiliary_head=dict(num_classes=5, ignore_index=ignore_index))
 
 default_hook = dict(checkpoint=dict(interval=10000))
